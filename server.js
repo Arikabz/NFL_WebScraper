a0702fa43a1c130c0554e6f75d9a6f3abedbce40
Arikabz <arturo.guza1999@gmail.com>
2022-10-23 21:56:30 -0500

    almost there

--- server.js
+++ server.js
@@ -5,40 +5,60 @@ const cheerio = require("cheerio");
 
 const app = express();
 
-async function webScraper() {
-    const url= "https://www.cbssports.com/nfl/schedule/2022/regular/8/";
+async function weekScrapper(num) {
+    const url= `https://www.cbssports.com/nfl/schedule/2022/regular/${num}/`;
     const dataArray = [];
     await axios(url).then((res)=>{
         const html_data = res.data;
         const $ = cheerio.load(html_data);
 
 
-        const diffSelector = 'div.TableBaseWrapper:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(3) > tr'
-        const keys = [
+        const diffSelector = 'div.TableBaseWrapper  tr'
+        const oldSelect = 'div.TableBaseWrapper:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1)'
+        const keysFuture = [
             'AwayLogo',
             'Away',
             'HomeLogo',
             'Home',
-            'Away',
-            'Home',
             'Time',
             'TV',
+            'Venue',
+            'Buy Tickets',
+        ];
+
+        const keysHappened = [
+            'AwayLogo',
+            'Away',
+            'HomeLogo',
+            'Home',
             'result',
         ];
+
 
         $(diffSelector).each((parentIndex, parentElem) => {
             let keyIndex = 0;
             const gameDetails = {};
-            if (parentIndex <=16){
+            if (parentIndex <=15){
+                    let keys;
+                    if($(parentElem).children().length>3){
+                        keys = keysFuture;
+                    }
+                    else{
+                        keys = keysHappened
+                    }
                 $(parentElem).children().each((childId, childElem) => {
-                    const value = $(childElem).text();
-                    console.log(value)
+                    console.log($(childElem).children().length>3)
+                    const value = $(childElem).text().trim();
                     const img = $(childElem).find('figure').find('img').attr('data-lazy');
-                    console.log(img)
-                    if(img&&value){
+                    if(childId<2){
                         gameDetails[keys[keyIndex]] = img;
-                        gameDetails[keys[keyIndex+1]] = value;
-                        keyIndex += 2
+                        keyIndex++ 
+                        gameDetails[keys[keyIndex]] = value;
+                        keyIndex++ 
+                    }
+                    else if (value){
+                        gameDetails[keys[keyIndex]] = value;
+                        keyIndex++ 
                     }
                 });
                 dataArray.push(gameDetails);
@@ -50,10 +70,17 @@ async function webScraper() {
 
 app.get("/api", async (req,res) => {
     try {
-        const games = await webScraper();
-        return res.status(200).json({
-            result: games,
-        });
+        let arr = []
+        for (let i = 1; i<=18; i++){
+            arr.push(i)
+        }
+        Promise.all(arr.map(x=>{
+            weekScrapper(x);
+        })).then((values) => {
+                return res.status(200).json({
+                    result: values,
+                })
+            })
     } catch (err) {
         return res.status(500).json({
             err: err.toString(),
